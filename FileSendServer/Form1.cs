eb3259d19b2a4c2b179850cfd0ab51d5d445bbb6
khaledsalem999 <khaledsalem999@gmail.com>
2017-04-26 19:34:33 +0200

    Add project files.

---
+++ FileSendServer/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace FileSendServer
+{
+    public partial class Form1 : Form
+    {
+
+        private ArrayList nSockets;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+            label1.Text = "My IP address is " + IPHost.AddressList[0].ToString();
+            nSockets = new ArrayList();
+            Thread thdListener = new Thread(new ThreadStart(listenerThread));
+            thdListener.Start();
+        }
+
+        public void listenerThread()
+        {
+            TcpListener tcpListener = new TcpListener(8080);
+            tcpListener.Start();
+            while (true)
+            {
+                Socket handlerSocket = tcpListener.AcceptSocket();
+                if (handlerSocket.Connected)
+                {
+                    Control.CheckForIllegalCrossThreadCalls = false;
+                    listBox1.Items.Add(
+                    handlerSocket.RemoteEndPoint.ToString() + " connected.");
+                    lock (this)
+                    {
+                        nSockets.Add(handlerSocket);
+                    }
+                    ThreadStart thdstHandler = new
+                    ThreadStart(handlerThread);
+                    Thread thdHandler = new Thread(thdstHandler);
+                    thdHandler.Start();
+                }
+            }
+        }
+
+        public void handlerThread()
+        {
+            Socket handlerSocket = (Socket)nSockets[nSockets.Count - 1];
+            NetworkStream networkStream = new NetworkStream(handlerSocket);
+            int thisRead = 0;
+            int blockSize = 1024;
+            Byte[] dataByte = new Byte[blockSize];
+            lock (this)
+            {
+                // Only one process can access
+                // the same file at any given time
+                Stream fileStream = File.OpenWrite("C:\\Users\\Khaled\\Documents\\SubmittedFile.txt");
+                while (true)
+                {
+                    thisRead = networkStream.Read(dataByte, 0, blockSize);
+                    fileStream.Write(dataByte, 0, thisRead);
+                    if (thisRead == 0) break;
+                }
+                fileStream.Close();
+            }
+            listBox1.Items.Add("File Written");
+            handlerSocket = null;
+        }
+
+    }
+}
