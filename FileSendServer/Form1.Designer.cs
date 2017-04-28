e43d953f765b9f9bd414930fdbfbd60d0f640c64
Peldorri <passant.eldorry@gmail.com>
2017-04-28 18:52:44 +0200

    changed almost kol haga

--- FileSendServer/Form1.Designer.cs
+++ FileSendServer/Form1.Designer.cs
@@ -35,28 +35,30 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Location = new System.Drawing.Point(9, 32);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(258, 196);
+            this.listBox1.Size = new System.Drawing.Size(194, 160);
             this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "My Address is : ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 249);
+            this.ClientSize = new System.Drawing.Size(212, 202);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- FileSendServer/Form1.cs
+++ FileSendServer/Form1.cs
@@ -20,7 +20,10 @@ namespace FileSendServer
     {
 
         private ArrayList nSockets;
-
+        private TcpListener tcpListener;
+        private Socket handlerSocket;
+        private string key = "123";
+        private bool isAuth = false;
         public Form1()
         {
             InitializeComponent();
@@ -30,18 +33,18 @@ namespace FileSendServer
         {
             IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
             label1.Text = "My IP address is " + IPHost.AddressList[0].ToString();
-            nSockets = new ArrayList();
-            Thread thdListener = new Thread(new ThreadStart(listenerThread));
-            thdListener.Start();
+            tcpListener = new TcpListener(8080);
+            Thread t = new Thread(new ThreadStart(commandHandler));
+            t.Start();
         }
 
         public void listenerThread()
         {
-            TcpListener tcpListener = new TcpListener(8080);
+            tcpListener = new TcpListener(8080);
             tcpListener.Start();
             while (true)
             {
-                Socket handlerSocket = tcpListener.AcceptSocket();
+                handlerSocket = tcpListener.AcceptSocket();
                 if (handlerSocket.Connected)
                 {
                     Control.CheckForIllegalCrossThreadCalls = false;
@@ -51,8 +54,7 @@ namespace FileSendServer
                     {
                         nSockets.Add(handlerSocket);
                     }
-                    ThreadStart thdstHandler = new
-                    ThreadStart(handlerThread);
+                    ThreadStart thdstHandler = new ThreadStart(handlerThread);
                     Thread thdHandler = new Thread(thdstHandler);
                     thdHandler.Start();
                 }
@@ -70,7 +72,7 @@ namespace FileSendServer
             {
                 // Only one process can access
                 // the same file at any given time
-                Stream fileStream = File.OpenWrite("C:\\Users\\Khaled\\Documents\\SubmittedFile.txt");
+                Stream fileStream = File.OpenWrite("C:\\Users\\user\\Documents\\SubmittedFsssile.txt");
                 while (true)
                 {
                     thisRead = networkStream.Read(dataByte, 0, blockSize);
@@ -80,8 +82,69 @@ namespace FileSendServer
                 fileStream.Close();
             }
             listBox1.Items.Add("File Written");
-            handlerSocket = null;
+            //handlerSocket = null;
+        }
+        public void sendString(NetworkStream ns, string str)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(str);
+            ns.Write(buffer, 0, buffer.Length);
+            ns.Flush();
+        }
+        public void commandHandler()
+        {
+            tcpListener.Start();
+            TcpClient client = tcpListener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            byte[] buffer; int bytesRead; string dataRecieved;
+            while (true)
+            {
+                buffer = new byte[client.ReceiveBufferSize];
+                bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+                dataRecieved = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                listBox1.BeginInvoke((Action)(() => listBox1.Items.Add(dataRecieved)));
+                if (dataRecieved.StartsWith("COMMAND_AUTH"))
+                {
+                    string sentKey = dataRecieved.Remove(0, "COMMAND_AUTH:".Length);
+                    if (sentKey == key) {
+                        isAuth = true;
+                        this.sendString(stream, "SUCCESS");
+                    } else
+                    {
+                        this.sendString(stream, "FAIL");
+                    }
+                }
+                else if (isAuth)
+                {
+                    if (dataRecieved.StartsWith("COMMAND_LIST"))
+                    {
+                        DirectoryInfo d = new DirectoryInfo(@"C:\Users\user\Documents");//Assuming Test is your Folder
+                        FileInfo[] Files = d.GetFiles(); //Getting Text files
+                        string str = "";
+                        foreach (FileInfo file in Files)
+                        {
+                            str = str + ", " + file.Name;
+                        }
+                        this.sendString(stream, str);
+                    }
+                    else if(dataRecieved.StartsWith("COMMAND_RECIEVE"))
+                    {
+                        string fileName = dataRecieved.Remove(0, "COMMAND_RECIEVE:".Length);
+                        if (File.Exists(@"C:\Users\user\Documents\" + fileName.Trim()))
+                        {
+                            byte[] fileBytes = File.ReadAllBytes(@"C:\Users\user\Documents\" + fileName.Trim());
+                            stream.Write(fileBytes, 0, fileBytes.Length);
+                            stream.Flush();
+                        }
+                    }
+                }
+                else
+                {
+                    this.sendString(stream, "UNAUTHORIZED");
+                }
+
+            }
         }
 
+      
     }
 }
