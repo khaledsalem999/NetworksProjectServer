55bc50633ec6bef429f9d64378144f526bfdbcb4
Peldorri <passant.eldorry@gmail.com>
2017-05-01 17:04:05 +0200

    Send Delete Files

--- FileSendServer/Form1.cs
+++ FileSendServer/Form1.cs
@@ -31,6 +31,7 @@ namespace FileSendServer
         public static extern void LockWorkStation();
         [DllImport("user32")]
         public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+        public static String logFileDir= "C:\\Users\\user\\networks2\\Log.txt";
         public Form1()
         {
             InitializeComponent();
@@ -79,7 +80,7 @@ namespace FileSendServer
             {
                 // Only one process can access
                 // the same file at any given time
-                Stream fileStream = File.OpenWrite("C:\\Users\\Khaled\\Documents\\SubmittedFsssile.txt");
+                Stream fileStream = File.OpenWrite("C:\\Users\\user\\Documents\\SubmittedFsssile.txt");
                 while (true)
                 {
                     thisRead = networkStream.Read(dataByte, 0, blockSize);
@@ -88,8 +89,8 @@ namespace FileSendServer
                 }
                 fileStream.Close();
             }
-            listBox1.Items.Add("File Written");
-            File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "FILE_RECEIVED " + DateTime.Now + Environment.NewLine);
+            listBox1.BeginInvoke((Action)(() => listBox1.Items.Add("File Written")));
+            File.AppendAllText(logFileDir, "FILE_RECEIVED " + DateTime.Now + Environment.NewLine);
             //handlerSocket = null;
         }
         public void sendString(NetworkStream ns, string str)
@@ -116,11 +117,11 @@ namespace FileSendServer
                     if (sentKey == key) {
                         isAuth = true;
                         this.sendString(stream, "SUCCESS");
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "COMMAND_AUTH - SUCCESS " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "COMMAND_AUTH - SUCCESS " + DateTime.Now + Environment.NewLine);
                     } else
                     {
                         this.sendString(stream, "FAIL");
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "COMMAND_AUTH - FAIL " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "COMMAND_AUTH - FAIL " + DateTime.Now + Environment.NewLine);
                     }
                     stream.Close();
                 }
@@ -128,7 +129,7 @@ namespace FileSendServer
                 {
                     if (dataRecieved.StartsWith("COMMAND_LIST"))
                     {
-                        DirectoryInfo d = new DirectoryInfo(@"C:\Users\Khaled\Documents");//Assuming Test is your Folder
+                        DirectoryInfo d = new DirectoryInfo(@"C:\Users\user\Documents");//Assuming Test is your Folder
                         FileInfo[] Files = d.GetFiles(); //Getting Text files
                         string str = "";
                         foreach (FileInfo file in Files)
@@ -137,36 +138,75 @@ namespace FileSendServer
                         }
                         this.sendString(stream, str);
                         stream.Close();
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "COMMAND_LIST " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "COMMAND_LIST " + DateTime.Now + Environment.NewLine);
                     }
                     else if(dataRecieved.StartsWith("COMMAND_RECIEVE"))
                     {
                         string fileName = dataRecieved.Remove(0, "COMMAND_RECIEVE:".Length);
-                        if (File.Exists(@"C:\Users\Khaled\Documents\" + fileName.Trim()))
+                        if (File.Exists(@"C:\Users\user\Documents\" + fileName.Trim()))
                         {
-                            Stream fileStream = File.OpenRead(@"C:\Users\Khaled\Documents\" + fileName.Trim());
+                            Stream fileStream = File.OpenRead(@"C:\Users\user\Documents\" + fileName.Trim());
                             byte[] fileBuffer = new byte[fileStream.Length];
                             fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
                             // Open a TCP/IP Connection and send the data
                             stream.Write(fileBuffer, 0, fileBuffer.GetLength(0));
                             stream.Close();
                         }
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "COMMAND_RECEIVE " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "COMMAND_RECEIVE " + fileName +"  " + DateTime.Now + Environment.NewLine);
                     }
                     else if(dataRecieved.StartsWith("SHUT_DOWN"))
                     {
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "SHUT_DOWN " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "SHUT_DOWN " + DateTime.Now + Environment.NewLine);
                         Process.Start("shutdown", "/s /t 10");
                     }
                     else if (dataRecieved.StartsWith("LOG_OFF"))
                     {
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "LOG_OFF " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "LOG_OFF " + DateTime.Now + Environment.NewLine);
                         ExitWindowsEx(0, 0);
                     }
                     else if (dataRecieved.StartsWith("RESTART"))
                     {
-                        File.AppendAllText("C:\\Users\\Khaled\\Documents\\Visual Studio 2015\\Projects\\FileSendServer\\FileSendServer\\Log.txt", "RESTART " + DateTime.Now + Environment.NewLine);
+                        File.AppendAllText(logFileDir, "RESTART " + DateTime.Now + Environment.NewLine);
                         Process.Start("shutdown", "/r /t 10");
+                    }
+                    else if (dataRecieved.StartsWith("COMMAND_DELETE"))
+                    {
+
+                        string fileNameD = dataRecieved.Remove(0, "COMMAND_DELETE".Length);
+                        if (File.Exists(@"C:\Users\user\Documents\" + fileNameD.Trim()))
+                        {
+                            File.Delete(@"C:\Users\user\Documents\" + fileNameD.Trim());
+                            this.sendString(stream, "SUCCESS");
+                        } else
+                        {
+                            this.sendString(stream, "FAIL");
+                        }
+                        File.AppendAllText(logFileDir, "Delete "+ fileNameD + "  "+ DateTime.Now + Environment.NewLine);
+                    }
+                    else if (dataRecieved.StartsWith("COMMAND_SEND"))
+                    {
+                        //Socket handlerSocket = (Socket)nSockets[nSockets.Count - 1];
+                        string fileNameS = dataRecieved.Remove(0, "COMMAND_SEND:".Length);
+                        this.sendString(stream, "COMMAND_SEND_ACKNOWLEGED");
+                        int thisRead = 0;
+                        int blockSize = 1024;
+                        Byte[] dataByte = new Byte[blockSize];
+                        lock (this)
+                        {
+                            // Only one process can access
+                            // the same file at any given time
+                            Stream fileStream1 = File.OpenWrite((@"C:\Users\user\networks2\" + fileNameS));
+                            while (true)
+                            {
+                                thisRead = stream.Read(dataByte, 0, blockSize);
+                                fileStream1.Write(dataByte, 0, thisRead);
+                                if (thisRead == 0) break;
+                            }
+                            fileStream1.Close();
+                        }
+                        listBox1.BeginInvoke((Action)(() => listBox1.Items.Add("File Written")));
+
+                        File.AppendAllText(logFileDir, "FILE_SENT "+ fileNameS + DateTime.Now + Environment.NewLine);
                     }
                 }
                 else
